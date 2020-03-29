4a4e0de7f13ca4d7dd6948ea3bd43e0ff649b2f3
David Durrant <ddurrant@halosight.com>
2020-03-28 19:47:38 -0600

    cars196 loads with tensorflow.

--- main.py
+++ main.py
@@ -2,22 +2,61 @@ import tensorflow as tf
 from tensorflow import keras
 import tensorflow_datasets as tfds
 
-data, info = tfds.load("cars196", with_info=True)
-train_data, test_data = data['train'], data['test']
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+import scipy.io
+#  image size
+SIZE = 256
+(ds_train, ds_test), ds_info = tfds.load(
+    'cars196',
+    split=['train', 'test'],
+    shuffle_files=True,
+    as_supervised=True,
+    with_info=True,
+)
 
 
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dense(10)
+def normalize_img(image, label):
+    """Normalizes images: `uint8` -> `float32`."""
+    image = tf.image.resize(image, [SIZE, SIZE])
+    return tf.cast(image, tf.float32) / 255., label
+
+
+# train data
+ds_train = ds_train.map(
+    normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+ds_train = ds_train.batch(128)
+ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
+
+# test data
+ds_test = ds_test.map(
+    normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_test = ds_test.batch(128)
+ds_test = ds_test.cache()
+ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(SIZE, SIZE, 3)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(196, activation='softmax')
 ])
+model.compile(
+    loss='sparse_categorical_crossentropy',
+    optimizer=tf.keras.optimizers.Adam(0.001),
+    metrics=['accuracy'],
+)
 
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+model.fit(
+    ds_train,
+    epochs=20,
+    validation_data=ds_test,
+)
 
-# model.fit(train_data, info.features['label'], epochs=10)
-#
-# test_loss, test_acc = model.evaluate(test_data, info.features['label'], verbose=2)
-#
-# print('\nTest accuracy:', test_acc)
+test_loss, test_acc = model.evaluate(ds_test, verbose=2)
+
+print('\nTest accuracy:', test_acc)
+print("end")
